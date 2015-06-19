2f46d0296a58392e3105d273cc3886b6edcd42fb
kleberksms <kleberksms@gmail.com>
2015-06-18 22:43:30 -0300

    Alteração na pasta App e service e Interfaces bases de repositorio e serviço

---
+++ Kairos.Domain/Services/BaseService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Kairos.Domain.Interfaces.Repositories;
+using Kairos.Domain.Interfaces.Services;
+
+namespace Kairos.Domain.Services
+{
+    public class BaseService<TEntity> : IDisposable, IBaseServices<TEntity> where TEntity : class
+    {
+
+        private readonly IBaseRepository<TEntity> _repository;
+
+        public BaseService(IBaseRepository<TEntity> repository)
+        {
+            _repository = repository;
+        }
+
+
+        public void Add(TEntity obj)
+        {
+            _repository.Add(obj);
+        }
+
+        public TEntity GetById(int id)
+        {
+            return _repository.GetById(id);
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return _repository.GetAll();
+        }
+
+        public void Update(TEntity obj)
+        {
+            _repository.Update(obj);
+        }
+
+        public void Remove(TEntity obj)
+        {
+            _repository.Remove(obj);
+        }
+
+        public void Dispose()
+        {
+            _repository.Dispose();
+        }
+    }
+}
