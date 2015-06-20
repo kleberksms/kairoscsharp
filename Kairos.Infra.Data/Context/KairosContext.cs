a2318d210466523ba4ae7e800c4f7d4d4946e805
kleberksms <kleberksms@gmail.com>
2015-06-20 15:47:37 -0300

    falta terminar o contexto e rever sobre tasks

---
+++ Kairos.Infra.Data/Context/KairosContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kairos.Domain.Entities;
+
+namespace Kairos.Infra.Data.Context
+{
+    public class KairosContext : DbContext
+    {
+        public KairosContext() : base("kairos"){}
+
+        public DbSet<Comment> Comments { get; set; }
+
+        public DbSet<Measure> Measures { get; set; }
+
+        public DbSet<Ranking> Rankings { get; set; }
+
+        public DbSet<Sprint> Sprints { get; set; }
+
+        public DbSet<Status> Status { get; set; }
+
+        public DbSet<Task> Tasks { get; set; }
+
+
+    }
+}
---
+++ Kairos.Infra.Data/Repositories/BaseRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kairos.Domain.Interfaces.Repositories;
+
+namespace Kairos.Infra.Data.Repositories
+{
+    public class BaseRepository<TEntity> : IDisposable, IBaseRepository<TEntity> where TEntity : class
+    {
+        public void Add(TEntiry obj)
+        {
+            Db.Set<TEntiry>().Add(obj);
+            Db.SaveChanges();
+        }
+
+        public TEntiry GetById(int id)
+        {
+            return Db.Set<TEntiry>().Find(id);
+        }
+
+        public IEnumerable<TEntiry> GetAll()
+        {
+            return Db.Set<TEntiry>().ToList();
+        }
+
+        public void Update(TEntiry obj)
+        {
+            Db.Entry(obj).State = EntityState.Modified;
+            Db.SaveChanges();
+        }
+
+        public void Remove(TEntiry obj)
+        {
+            Db.Set<TEntiry>().Remove(obj);
+            Db.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
