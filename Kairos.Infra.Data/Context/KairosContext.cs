89e897ed7f6bf817f0c8aa1718279e227cc0d8b7
kleberksms <kleberksms@gmail.com>
2015-06-24 08:05:30 -0300

    Classes de repositorio

--- Kairos.Infra.Data/Context/KairosContext.cs
+++ Kairos.Infra.Data/Context/KairosContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -30,5 +31,48 @@ namespace Kairos.Infra.Data.Context
 
         public DbSet<Timesheet> Timesheet { get; set; }
 
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+            modelBuilder.Properties()
+                .Where(p => p.Name.Equals("Id"))
+                .Configure(p => p.IsKey());
+
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasColumnType("varchar"));
+
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasMaxLength(75));
+
+            //modelBuilder.Configurations.Add(new EntityConfiguration());
+        }
+
+        public override int SaveChanges()
+        {
+            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("Created") != null))
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Property("Created").CurrentValue = DateTime.Now;
+                }
+
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Property("Created").IsModified = false;
+                }
+
+            }
+
+            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("Modified") != null))
+            {
+                entry.Property("Modified").CurrentValue = DateTime.Now;
+            }
+
+
+            return base.SaveChanges();
+        }
     }
 }
---
+++ Kairos.Infra.Data/Repositories/TimesheetRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kairos.Infra.Data.Repositories
+{
+    class TimesheetRepository
+    {
+    }
+}
