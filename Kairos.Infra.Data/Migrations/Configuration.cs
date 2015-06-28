e9ce8e38fbd00c27801eb4eed5dde1db3ff961ad
kleberksms <kleberksms@gmail.com>
2015-06-28 11:58:27 -0300

    css e js files

--- Kairos.Infra.Data/Migrations/Configuration.cs
+++ Kairos.Infra.Data/Migrations/Configuration.cs
@@ -5,14 +5,14 @@ namespace Kairos.Infra.Data.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<Kairos.Infra.Data.Context.KairosContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<Context.KairosContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
         }
 
-        protected override void Seed(Kairos.Infra.Data.Context.KairosContext context)
+        protected override void Seed(Context.KairosContext context)
         {
             //  This method will be called after migrating to the latest version.
 
